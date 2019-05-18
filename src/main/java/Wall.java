55ca5d5784ea8f2da3b418037a637cd25a73f8af
Francisco Delgado <fdelgado@itba.edu.ar>
2019-05-18 15:30:20 -0300

    Add wall model

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,2 +1,47 @@
 public class Wall {
+
+    public double initialX, initialY, finalX, finalY, enx, eny, m, c;
+
+    public Wall(double initialX, double initialY, double finalX, double finalY) {
+        this.initialX = initialX;
+        this.initialY = initialY;
+        this.finalX = finalX;
+        this.finalY = finalY;
+        this.m = finalY - initialY / finalX - initialX;
+        this.c = initialY - m*initialX;
+
+        double magnitude = Math.sqrt(Math.pow(finalX - initialX,  2) + Math.pow(finalY - initialY, 2));
+        this.enx = -(finalY - initialY)/magnitude;
+        this.eny = (finalX - initialX)/magnitude;
+    }
+
+    private double centerToWall(Particle particle) {
+        if(
+                Math.sqrt(Math.pow(particle.x - initialX,  2) + Math.pow(particle.y - initialY, 2)) > particle.r ||
+                Math.sqrt(Math.pow(particle.x - finalX,  2) + Math.pow(particle.y - finalY, 2)) > particle.r
+        ){
+            return particle.r+1;
+        }
+
+        if(initialX == finalX){
+            return particle.x -initialX;
+        }
+
+        if(initialY == finalY) {
+            return particle.y -initialY;
+        }
+
+        return Math.sqrt(Math.pow(particle.x - ((particle.y - c) / m),  2) + Math.pow(particle.y - (m*particle.x + c), 2));
+    }
+
+    public double getOverlap(Particle particle){
+        double overlap = particle.r - centerToWall(particle);
+        return overlap >= 0 ? overlap : 0;
+    }
+
+    public double getNormalRelVel(Particle particle) {
+        return particle.vx  * enx + particle.vy * eny;
+    }
+
+
 }
